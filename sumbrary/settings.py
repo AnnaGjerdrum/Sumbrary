31235cd1fdc956e3c178ab5d3fff21577e35a831
anonymouswebprogrammer <b.a.peters@student.tue.nl>
2018-01-13 13:57:20 +0100

    blabla

--- sumbrary/settings.py
+++ sumbrary/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
+# SECURITY WARNING: keep the secret kedfy used in production secret!
 SECRET_KEY = '+x(6+ok9krj+kt)j_ka!0s!-$_7o5^$24t10_1&e3)10%c_lgt'
 
 # SECURITY WARNING: don't run with debug turned on in production!
